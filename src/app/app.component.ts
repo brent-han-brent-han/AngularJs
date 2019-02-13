7773447ffddaf59ce34651078b085e8bad050018
leonpater <fhanissc@live.com>
2019-02-13 16:04:39 +0800

    learing scd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Test Auction';
+  test() {
+    $('xxxx').show();
+    $('yyyy').show();
+  }
 }
